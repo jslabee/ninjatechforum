652978cdbaf56b6c88e238c3dd7d1b5db5f354cc
jslabee <jslabe@gmail.com>
2018-02-08 09:15:21 +0100

    Move handlers out of main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import webapp2
+from handlers.base import MainHandler, CookieHandler
+
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler, name="main-page"),
+    webapp2.Route('/set-cookie', CookieHandler),
+], debug=True)
